878cff9283baa725fdcdf1aa07f45c6657fc6dca
briask <briask247@gmail.com>
2015-11-29 16:41:48 +0000

    Added ErrorLocation Closes #3

--- FileProcessor/BatchFileProcessor.cs
+++ FileProcessor/BatchFileProcessor.cs
@@ -1,40 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.IO;
-using System.Linq;
-using System.Text;
-
-namespace FileProcessor
+﻿namespace FileProcessor
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using log4net;
+
     public class BatchFileProcessor
     {
         private string unprocessedFilesPath;
         private string processedFilesPath;
+        private string errorPath;
 
         private List<string> successfullyProcessedFiles;
         private List<string> unsuccessfullyProcessedFiles;
 
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
+        public static ILog Log
+        {
+            get
+            {
+                return log;
+            }
+        }
+
         private BatchFileProcessor()
         {
         }
 
-        public BatchFileProcessor(string unprocessedFilesPath, string processedFilesPath)
+        public BatchFileProcessor(string unprocessedFilesPath, string processedFilesPath, string errorPath)
         {
             if (string.IsNullOrWhiteSpace(unprocessedFilesPath))
             {
-                throw new ArgumentNullException("unprocessedFilesPath");
+                throw new ArgumentNullException(nameof(unprocessedFilesPath));
             }
 
             if (string.IsNullOrWhiteSpace(processedFilesPath))
             {
-                throw new ArgumentNullException("processedFilesPath");
+                throw new ArgumentNullException(nameof(processedFilesPath));
+            }
+
+            if (string.IsNullOrWhiteSpace(errorPath))
+            {
+                throw new ArgumentNullException(nameof(errorPath));
             }
 
             this.unprocessedFilesPath = unprocessedFilesPath;
             this.processedFilesPath = processedFilesPath;
+            this.errorPath = errorPath;
         }
 
         public IDictionary<string, DataSet> ProcessAllFiles(IFileProcessor fileProcessor)
@@ -98,28 +114,79 @@ namespace FileProcessor
             try
             {
                 DataSet data = fileProcessor.ProcessFile(fullFileName);
-                if (data.Tables.Count > 0)
+                if (data != null && data.Tables.Count > 0)
                 {
                     bool successfulMove = MoveFileToProcessedLocation(fullFileName);
                     if (successfulMove)
                     {
                         data.DataSetName = fullFileName;
-                        log.InfoFormat("Success");
+                        Log.DebugFormat("Success");
                         return data;
                     }
                     else
                     {
-                        log.ErrorFormat("Unable to process file {0}", fullFileName);
+                        Log.ErrorFormat("Unable to process file {0}", fullFileName);
+                        return new DataSet();
+                    }
+                }
+                else
+                {
+                    bool successfulMove = MoveFileToErrorLocation(fullFileName);
+                    if (successfulMove)
+                    {
+                        data.DataSetName = fullFileName;
+                        Log.DebugFormat("Success");
+                        return data;
+                    }
+                    else
+                    {
+                        Log.ErrorFormat("Unable to process file {0}", fullFileName);
                         return new DataSet();
                     }
                 }
             }
             catch (Exception ex)
             {
-                log.ErrorFormat("Unable to process file {0} {1}", filename, ex);
+                Log.ErrorFormat("Unable to process file {0} {1}", filename, ex);
             }
 
             return new DataSet();
+        }
+
+        private bool MoveFileToErrorLocation(string filename)
+        {
+            try
+            {
+                var originalfile = new FileInfo(filename);
+                string originalFilename = originalfile.Name;
+
+                var processedTime = DateTime.Now.ToString("yyyy-MM-ddThhmmss");
+                var newFileName = string.Format("{0}.{1}", originalFilename, processedTime);
+
+                var newFileLocation = Path.Combine(this.errorPath, newFileName);
+                if (File.Exists(newFileLocation))
+                {
+                    processedTime = DateTime.Now.ToString("yyyy-MM-ddThhmmss.fff");
+                    newFileName = string.Format("{0}.{1}", originalFilename, processedTime);
+                    newFileLocation = Path.Combine(this.errorPath, newFileName);
+
+                    if (File.Exists(newFileLocation))
+                    {
+                        newFileName = string.Format("{0}.{1}", originalFilename, Guid.NewGuid().ToString("N"));
+                        newFileLocation = Path.Combine(this.processedFilesPath, newFileName);
+                    }
+                }
+
+                originalfile.MoveTo(newFileLocation);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal("MoveFileToProcessedLocation error : ", ex);
+            }
+
+            return false;
         }
 
         private bool MoveFileToProcessedLocation(string filename)
@@ -152,7 +219,7 @@ namespace FileProcessor
             }
             catch (Exception ex)
             {
-                log.Fatal("MoveFileToProcessedLocation error : ", ex);
+                Log.Fatal("MoveFileToProcessedLocation error : ", ex);
             }
 
             return false;
